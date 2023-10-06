6aa941b0405959edb946ea681866dfad00f5d25a
kweku-xvi <nksarps@gmail.com>
2023-10-06 21:10:07 +0000

    project's base app

--- user_creation/urls.py
+++ user_creation/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('profile/', register_views.profile, name='profile'),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
